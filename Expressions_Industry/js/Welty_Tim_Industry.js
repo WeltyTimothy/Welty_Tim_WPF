88fbd3b1b791b8bb90c532d21c8ea7cc4721ec6c
Tim Welty <weltytim@yahoo.com>
2013-11-07 17:44:23 -0500

    Added more folders

---
+++ Expressions_Industry/js/Welty_Tim_Industry.js
@@ -0,0 +1,9 @@
+// Timothy Welty 11/07/2013
+
+var width = prompt("We are calculating the area of a TRANE FURNACE. Please enter the width");
+var height = prompt("Please enter the height of the TRANE FURNACE.");
+var depth = prompt("Please enter the depth of the TRANE FURNACE.");
+var volume = width * height * depth;//calculates with that info is going be
+console.log(volume);
+var result = "The Volume of your TRANE FURNACE is " + volume + " sq feet"
+alert(result);

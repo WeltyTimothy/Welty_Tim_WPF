f5066a4fb0a78306aede3a6dbe4f29f65585b3fa
Tim Welty <weltytim@yahoo.com>
2013-11-07 18:37:48 -0500

    added string variables and arrarys

--- Expressions_Wacky/js/Welty_Tim_Expressions_wacky.js
+++ Expressions_Wacky/js/Welty_Tim_Expressions_wacky.js
@@ -3,7 +3,8 @@ var owls = ["Owls fit on the roller coaster"]
 var amount = prompt("We are making a roller coaster for owls! Please tell us how many seats are in the roller coaster");
 var cars = prompt("Please enter how many cars are there.");
 var trains = prompt("Please enter how many trains are able to run at one time.");
-var quantity = amount * cars * trains;//calculates with that info is going be calculated
+var owl = ["Barn Owl", "Spotted Owl"]
+var quantity = amount * cars * trains + owl[1];//calculates with that info is going be calculated
 console.log(quantity, owls[0]);
-var result = "" + quantity + " owls can fit on this roller coaster! Flip what the zip! That's a lot of owls!"
+var result = "" + quantity + "" + " can fit on this roller coaster! Flip what the zip! That's a lot of owls!"
 alert(result);

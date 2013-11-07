22b9d97077825a362be6280b7d0c58e4e0193cf9
Tim Welty <weltytim@yahoo.com>
2013-11-07 18:25:35 -0500

    Added wacky

--- Expressions_Wacky/Welty_Tim_Expressions_wacky.js
+++ Expressions_Wacky/Welty_Tim_Expressions_wacky.js
@@ -5,5 +5,5 @@ var cars = prompt("Please enter how many cars are there.");
 var trains = prompt("Please enter how many trains are able to run at one time.");
 var quantity = amount * cars * trains;//calculates with that info is going be calculated
 console.log(price, diapers[0]);
-var result = " " + quantity + " of owls can fit on this roller coaster! Flip what the zip! That's a lot of owls!"
+var result = "" + quantity + " of owls can fit on this roller coaster! Flip what the zip! That's a lot of owls!"
 alert(result);

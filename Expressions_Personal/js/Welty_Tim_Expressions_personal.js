c604bba41f87cab1660eaf151888aa6a7235a13c
Tim Welty <weltytim@yahoo.com>
2013-11-07 17:44:55 -0500

    Took our files to organize and file

--- Expressions_Personal/js/Welty_Tim_Expressions_personal.js
+++
@@ -1,9 +0,0 @@
-// Prompt and Alert
-
-var width = prompt("We are calculating how amazing this diaper bin is. Please enter the width");
-var height = prompt("Please enter the height of the Diaper bin.");
-var depth = prompt("Please enter the depth of the TRANE FURNACE.");
-var volume = width * height * depth;//calculates with that info is going be
-console.log(volume);
-var result = "The Volume of your TRANE FURNACE is " + volume + " sq feet"
-alert(result);

9375442c6bfee1b902cc680e4c64548049e8e125
Tim Welty <weltytim@yahoo.com>
2013-11-13 19:34:09 -0500

    changed wacky to a teritary form

--- conditionasl_wacky/js/conditionals.js
+++ conditionasl_wacky/js/conditionals.js
@@ -1,18 +1,14 @@
 // Timothy Welty 11/13/2013
 
-var evaporation = ["humidity"]
-var fear = prompt("How many ducks will scare you?");
+var ducks = prompt("How many ducks do you see outside");
 var attack = prompt("How many ducks can you take on by yourself?");
-var quantity = prompt("How many do you see outside?");
-var total = quantity - attack ;//calculates what that info is going be
- 	if(quantity - attack >= fear ){ 
-		//this will print out if the code is false
-		console.log("TOO MANY DUCKS!") 
-}
-	else{//this will print out if the code is true
-		console.log("It is safe to go outside...for now");
-	}
 
-var result = "There are " + total + " ducks outside. You better take them out quickly!"
-console.log(total, evaporation[0]);
-alert(result);
+//if the duck limit is under 100, we can go outside
+if (ducks - attack >= ducks){
+		console.log("TOO MANY DUCKS! STAY SAFE INDOORS");
+}
+	else{
+		console.log("It is safe to go outside...for now!");
+}
+
+(ducks - attack <= ducks) ? console.log("TOO MANY DUCKS! STAY SAFE INDOORS") : console.log("It is safe to go outside...for now");

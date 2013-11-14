79301cf574f4297cec285bda26a83f992af06dee
Tim Welty <weltytim@yahoo.com>
2013-11-13 19:07:14 -0500

    added more classwork/homework

--- conditionasl_industry/js/conditionals.js
+++ conditionasl_industry/js/conditionals.js
@@ -1,19 +1,17 @@
 // Timothy Welty 11/13/2013
-var furnHeight = 47;
-var padding = 4;
-var limit = 50;
-	if(furnHeight + padding >= limit){
-		//code preformed if the statement is true
-		console.log("Sorry the furnace is too big! You can't fit that here"); 
+var furnHeight = prompt("Please enter the height of the FURNACE COIL"); //telling me what the height of everything is
+var padding = prompt("Please enter the height of the FURNACE PADDING");
+var limit = prompt("Please enter the MAXIMUM HEIGHT allowed for Furnace placement"); //telling me what the limit will be for the job
+var total = furnHeight + padding;
+	if(furnHeight + padding >= limit){ 
+		//this will print out if the code is false
+		console.log("Sorry the coil won't fit there! it is", total - limit, "inches too big") 
 }
-	else{
+	else{//this will print out if the code is true
 		console.log("Awesome! We can fit that right in there. It'll be tight though");
 	}
-
-
-var height = prompt("Please enter the hieght of the furnace coil");
-var padding = prompt("Please enter the height of the furnace padding.");
-var depth = prompt("Please enter the depth of the TRANE FURNACE.");
-console.log(volume, squareFeet[0]);
-var result = "The Volume of your TRANE FURNACE is " + volume + " sq feet"
+var result = "The total height of your Furnace Coil is " + total + //for some reason this is showing as undefined...when I have it defined.
 alert(result);
+
+
+

843c48c59352893520e43e80a189814c96152ae1
Tim Welty <weltytim@yahoo.com>
2013-11-13 19:10:49 -0500

    Added personal conditonal statements

--- conditionasl_personal/js/conditionals.js
+++ conditionasl_personal/js/conditionals.js
@@ -1,16 +1,18 @@
 // Timothy Welty 11/13/2013
-var evaporation = [""]
+var evaporation = ["humidity"]
 var heat = prompt("what is the temperature now?");
 var time = prompt("What time is it?.");
 var humidity = prompt("How humid is it outside?");
+var allowed = 100
 var total = heat && time * humidity;//calculates what that info is going be
- 	if(time && heat * humidity ){ 
+ 	if(time && heat * humidity >= allowed){ 
 		//this will print out if the code is false
 		console.log("It is too muggy go back inside!") 
 }
 	else{//this will print out if the code is true
-		console.log("Awesome! We can fit that right in there. It'll be tight though");
+		console.log("Well lets go party at the beach now!");
 	}
+
+var result = "It is % " + total + " humid"
 console.log(total, evaporation[0]);
-var result = "The price of your yearly diaper buying is $ " + price + " a year"
 alert(result);

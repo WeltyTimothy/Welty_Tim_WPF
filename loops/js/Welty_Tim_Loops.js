cc9dab3a07d63be21bea9c29d84060dbb7f15be8
Tim Welty <weltytim@yahoo.com>
2013-11-17 11:39:56 -0500

    Adding looping folder

---
+++ loops/js/Welty_Tim_Loops.js
@@ -0,0 +1,9 @@
+// looping
+
+var b = 99 // sets up the index
+
+	while ( b > 0 ) { //checks the condition
+		console.log(b +  ' kegs on the wall');
+		b--;// counts down or up (incriment/detriment) a while loop
+	}
+

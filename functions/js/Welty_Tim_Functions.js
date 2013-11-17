7a2cd84483e000948dd9666f7c409c233c232cd7
Tim Welty <weltytim@yahoo.com>
2013-11-17 12:03:58 -0500

    Function Ivocation

--- functions/js/Welty_Tim_Functions.js
+++ functions/js/Welty_Tim_Functions.js
@@ -1,7 +1,7 @@
 //functions procedures and anonymous functions
 
 
-// basic function 
+// Basic Function 
 /* function functionName(){
 	code the function runs
 }*/
@@ -15,4 +15,32 @@ function calcArea(){
 	var height = 30;
 	var area = width * height;
 	console.log(area + 'square feet');
+}
+
+//Function Invocation
+
+function outptMsg(){
+	console.log('hello world');
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+}
+
+calcArea();
+calcArea();
+calcArea();
+
+//Variable Scope
+
+function outptMsg(){
+	console.log('hello world');
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
 }

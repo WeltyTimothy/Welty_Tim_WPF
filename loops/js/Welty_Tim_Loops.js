e50eb71be876c7476764b6285a4b4f6a7d826660
Tim Welty <weltytim@yahoo.com>
2013-11-17 11:46:43 -0500

    added more looping functions

--- loops/js/Welty_Tim_Loops.js
+++ loops/js/Welty_Tim_Loops.js
@@ -7,3 +7,17 @@ var b = 99 // sets up the index
 		b--;// counts down or up (incriment/detriment) a while loop
 	}
 
+var c = 10//Do While loop
+	
+	do {//DO run this code
+		console.log( c + 'kegs on the wall');
+		c--;
+	}
+	while (c > 0);//WHILE this code is true or yes
+	
+	
+//for loop
+//for (var is equal and var is greater than x minus one until 0)
+	for (var i = 10; i > 0; i--){
+		console.log(i + 'kegs on the wall')
+	}

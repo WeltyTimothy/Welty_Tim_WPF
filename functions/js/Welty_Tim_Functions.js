dfc2462e11b31d6b4fc94d4322bdedafa9c80842
Tim Welty <weltytim@yahoo.com>
2013-11-17 11:58:04 -0500

    Basic functions

--- functions/js/Welty_Tim_Functions.js
+++ functions/js/Welty_Tim_Functions.js
@@ -1,25 +1,18 @@
-// looping
+//functions procedures and anonymous functions
 
-var b = 99 // sets up the index
 
-	while ( b > 0 ) { //checks the condition
-		console.log(b +  ' kegs on the wall');
-		b--;// counts down or up (incriment/detriment) a while loop
-	}
+// basic function 
+/* function functionName(){
+	code the function runs
+}*/
 
-var c = 10//Do While loop
-	
-	do {//DO run this code
-		console.log( c + 'kegs on the wall');
-		c--;
-	}
-	while (c > 0);//WHILE this code is true or yes
-	
-	
-//for loop
-//for (var is equal and var is greater than x minus one until 0)
-	for (var i = 10; i > 0; i--){
-		console.log(i + 'kegs on the wall')
-	}
-	
-//
+function outptMsg(){
+	console.log('hello world');
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area + 'square feet');
+}
